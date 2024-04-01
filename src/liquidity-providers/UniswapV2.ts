4d4a3c86ee01524d46165cbfe3a0b5357f22e776
rouzwelt <rouzweltt@gmail.com>
2024-04-01 19:39:55 +0000

    Update UniswapV2.ts

--- src/liquidity-providers/UniswapV2.ts
+++ src/liquidity-providers/UniswapV2.ts
@@ -11,9 +11,19 @@ export class UniswapV2Provider extends UniswapV2BaseProvider {
   ) {
     const factory = {
       [ChainId.ETHEREUM]: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+      [ChainId.POLYGON]: '0x9e5A52f57b3038F1B8EeE45F28b3C1967e22799C',
+      [ChainId.ARBITRUM]: '0x6EcCab422D763aC031210895C81787E87B43A652',
+      [ChainId.BSC]: '0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6',
+      [ChainId.AVALANCHE]: '0x9e5A52f57b3038F1B8EeE45F28b3C1967e22799C',
+      [ChainId.OPTIMISM]: '0x0c3c1c532F1e39EdF36BE9Fe0bE1410313E074Bf',
     } as const
     const initCodeHash = {
       [ChainId.ETHEREUM]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+      [ChainId.POLYGON]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+      [ChainId.ARBITRUM]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+      [ChainId.BSC]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+      [ChainId.AVALANCHE]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+      [ChainId.OPTIMISM]: '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
     } as const
     super(chainId, web3Client, factory, initCodeHash, 
       // databaseClient
