e89f82eeef72ee71cbd7714255d1d924ce80b828
rouzwelt <rouzweltt@gmail.com>
2024-04-13 02:08:15 +0000

    Update TinesToRouteProcessor2.ts

--- src/TinesToRouteProcessor2.ts
+++ src/TinesToRouteProcessor2.ts
@@ -2,7 +2,7 @@ import { Hex } from 'viem'
 import { ChainId } from './chain'
 import { MultiRoute, RToken, RouteLeg, RouteStatus } from './tines'
 import { HEXer } from './HEXer'
-import { PoolCode } from './pool/PoolCode'
+import { PoolCode } from './pools/PoolCode'
 
 export enum TokenType {
   NATIVE = 'NATIVE',
